4c350d5ac707471eabe4169bdde1a1234e53e333
피수영 <vltndud123@naver.com>
2023-09-28 00:13:59 +0900

    Shape it to execute commands from CLI
    Cardgo.toml: 빌드 파일명 설정 및 CLI 인자 파싱 라이브러리 `clap` 추가
    src/main.rc: 인자 정의 및 파싱 로직 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::thread;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+
+    #[arg()]
+    http_method: String,
+    #[arg()]
+    url: String,
+    #[arg(short = 'r', long = "request")]
+    request_count: u32,
+    #[arg(short = 't', long = "thread")]
+    thread_count: u32,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
+fn main() {
+    let args = Args::parse();
+}
+
